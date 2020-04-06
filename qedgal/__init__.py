7e04ddd577bf4bc0d1ca33afaf4eebd0bcc0239a
Xurvan <xurvan@tuta.io>
2020-04-06 00:24:21 -0700

    Add package

---
+++ qedgal/__init__.py
@@ -0,0 +1,3 @@
+from .qedgal import Qedgal
+
+__version__ = '0.1.0.dev'
---
+++ qedgal/qedgal.py
@@ -0,0 +1,92 @@
+from patabase import Postgres
+
+
+class Qedgal(object):
+    def __init__(self, user: str, password: str, database: str, host: str = 'localhost', port=5432):
+        self._db = Postgres(
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            database=database)
+
+    def add(self, table: str, **parameters: any) -> None:
+        placeholders = ['%s' for _ in parameters]
+
+        sql = f'''
+            insert into {table} ({', '.join(parameters)})
+            values ({', '.join(placeholders)})
+        '''
+
+        self._db.perform(sql, *parameters.values())
+
+    def edit(self, table: str, pk: int, **parameters: any) -> None:
+        fields = [f'{key} = %s' for key in parameters if parameters[key]]
+        values = [parameters[key] for key in parameters if parameters[key]]
+
+        sql = f'''
+            update {table}
+            set {', '.join(fields)}
+            where id = %s
+        '''
+
+        self._db.perform(sql, *values, pk)  # TODO: check if "pk" exists
+
+    def delete(self, table: str, pk: int) -> None:
+        sql = f'''
+            delete
+            from {table}
+            where id = %s
+        '''
+
+        self._db.perform(sql, pk)  # TODO: check if "pk" exists
+
+    def list(self, table: str, user_id: int = None) -> list:
+        sql = f'''
+            select * 
+            from {table}_facade
+        '''
+
+        if user_id:
+            sql += f' where {user_id} = any(user_ids)'
+
+        return list(self._db.select(sql))
+
+    def get(self, table: str, pk: int, user_id: int = None) -> dict:
+        sql = f'''
+            select * 
+            from {table}_facade
+            where id = %s
+        '''
+
+        if user_id:
+            sql += f' and {user_id} = any(user_ids)'
+
+        return next(self._db.select(sql, pk), dict())
+
+    def query(self, table: str, q: str, fields: list, user_id: int = None) -> list:
+        filters = [f"{key}::varchar like %s" for key in fields]
+        values = [f'%{q}%' for _ in fields]
+
+        sql = f'''
+            select *
+            from {table}_facade
+            where {' or '.join(filters)}
+        '''
+
+        if user_id:
+            sql += f' and {user_id} = any(user_ids)'
+
+        return list(self._db.select(sql, *values))
+
+    def perform(self, sql: str, *args: any) -> int:
+        return self._db.perform(sql, *args)
+
+    def select(self, sql: str, *args: any) -> iter:
+        return self._db.select(sql, *args)
+
+    def procedure(self, func_name: str, **parameters: any) -> int:
+        return self._db.procedure(func_name, **parameters)
+
+    def function(self, func_name: str, **parameters: any) -> iter:
+        return self._db.function(func_name, **parameters)
